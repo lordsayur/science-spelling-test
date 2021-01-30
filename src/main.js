2af4c06b99c43fc013de525115e0f936d3b0a1e5
Lord Sayur <umar_317@hotmail.com>
2021-01-30 11:29:04 +0800

    feat: install animate.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'animate.css';
 import './index.css'
 
 createApp(App).use(store).use(router).mount('#app')
