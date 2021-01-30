4099633c955d7434ed355cb9ecd699f3dfcf33ac
Lord Sayur <umar_317@hotmail.com>
2021-01-30 11:41:32 +0800

    feat: add questions data

---
+++ src/store/modules/data.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    id: 1,
+    letters: "head",
+  },
+  {
+    id: 2,
+    letters: "hand",
+  },
+  {
+    id: 3,
+    letters: "shoulder",
+  },
+  {
+    id: 4,
+    letters: "elbow",
+  },
+  {
+    id: 5,
+    letters: "fingers",
+  },
+  {
+    id: 6,
+    letters: "knee",
+  },
+  {
+    id: 7,
+    letters: "toes",
+  },
+  {
+    id: 8,
+    letters: "feet",
+  },
+  {
+    id: 9,
+    letters: "eyebrow",
+  },
+  {
+    id: 10,
+    letters: "cheek",
+  },
+  {
+    id: 11,
+    letters: "lips",
+  },
+  {
+    id: 12,
+    letters: "chin",
+  },
+  {
+    id: 13,
+    letters: "forehead",
+  },
+  {
+    id: 14,
+    letters: "neck",
+  },
+  {
+    id: 15,
+    letters: "hair",
+  },
+  {
+    id: 16,
+    letters: "face",
+  },
+  {
+    id: 17,
+    letters: "salty",
+  },
+  {
+    id: 18,
+    letters: "spicy",
+  },
+  {
+    id: 19,
+    letters: "sour",
+  },
+  {
+    id: 20,
+    letters: "tongue",
+  },
+  {
+    id: 21,
+    letters: "soft",
+  },
+  {
+    id: 22,
+    letters: "taste",
+  },
+  {
+    id: 23,
+    letters: "loud",
+  },
+  {
+    id: 24,
+    letters: "sweet",
+  },
+]
