571065457bb6fe7f9c49407b4a8b8f157471e07b
Lord Sayur <umar_317@hotmail.com>
2021-02-08 22:25:34 +0800

    feat: add more quizzes and questions

--- src/store/modules/data.js
+++ src/store/modules/data.js
@@ -95,4 +95,256 @@ export default [
     id: 24,
     letters: "sweet",
   },
+  {
+    id: 25,
+    letters: "smooth",
+  },
+  {
+    id: 26,
+    letters: "bitter",
+  },
+  {
+    id: 27,
+    letters: "hard",
+  },
+  {
+    id: 28,
+    letters: "rough",
+  },
+  {
+    id: 29,
+    letters: "similar",
+  },
+  {
+    id: 30,
+    letters: "bad",
+  },
+  {
+    id: 31,
+    letters: "nice",
+  },
+  {
+    id: 32,
+    letters: "different",
+  },
+  {
+    id: 33,
+    letters: "water",
+  },
+  {
+    id: 34,
+    letters: "air",
+  },
+  {
+    id: 35,
+    letters: "food",
+  },
+  {
+    id: 36,
+    letters: "move",
+  },
+  {
+    id: 37,
+    letters: "grow",
+  },
+  {
+    id: 38,
+    letters: "reproduce",
+  },
+  {
+    id: 39,
+    letters: "non-living",
+  },
+  {
+    id: 40,
+    letters: "alive",
+  },
+  {
+    id: 41,
+    letters: "animal",
+  },
+  {
+    id: 42,
+    letters: "adult",
+  },
+  {
+    id: 43,
+    letters: "people",
+  },
+  {
+    id: 44,
+    letters: "plant",
+  },
+  {
+    id: 45,
+    letters: "young",
+  },
+  {
+    id: 46,
+    letters: "breathe",
+  },
+  {
+    id: 47,
+    letters: "eat",
+  },
+  {
+    id: 48,
+    letters: "living",
+  },
+  {
+    id: 49,
+    letters: "plant",
+  },
+  {
+    id: 50,
+    letters: "leaf",
+  },
+  {
+    id: 51,
+    letters: "leaves",
+  },
+  {
+    id: 52,
+    letters: "flower",
+  },
+  {
+    id: 53,
+    letters: "fruit",
+  },
+  {
+    id: 54,
+    letters: "stem",
+  },
+  {
+    id: 55,
+    letters: "roots",
+  },
+  {
+    id: 56,
+    letters: "protect",
+  },
+  {
+    id: 57,
+    letters: "hold",
+  },
+  {
+    id: 58,
+    letters: "sunlight",
+  },
+  {
+    id: 59,
+    letters: "absorb",
+  },
+  {
+    id: 60,
+    letters: "soil",
+  },
+  {
+    id: 61,
+    letters: "bud",
+  },
+  {
+    id: 62,
+    letters: "produce",
+  },
+  {
+    id: 63,
+    letters: "seed",
+  },
+  {
+    id: 64,
+    letters: "colour",
+  },
+  {
+    id: 65,
+    letters: "primary",
+  },
+  {
+    id: 66,
+    letters: "red",
+  },
+  {
+    id: 67,
+    letters: "yellow",
+  },
+  {
+    id: 68,
+    letters: "blue",
+  },
+  {
+    id: 69,
+    letters: "secondary",
+  },
+  {
+    id: 70,
+    letters: "orange",
+  },
+  {
+    id: 71,
+    letters: "green",
+  },
+  {
+    id: 72,
+    letters: "purple",
+  },
+  {
+    id: 73,
+    letters: "pink",
+  },
+  {
+    id: 74,
+    letters: "grey",
+  },
+  {
+    id: 75,
+    letters: "brown",
+  },
+  {
+    id: 76,
+    letters: "rainbow",
+  },
+  {
+    id: 77,
+    letters: "indigo",
+  },
+  {
+    id: 78,
+    letters: "violet",
+  },
+  {
+    id: 79,
+    letters: "mix",
+  },
+  {
+    id: 80,
+    letters: "hygiene",
+  },
+  {
+    id: 81,
+    letters: "healthy",
+  },
+  {
+    id: 82,
+    letters: "unhealthy",
+  },
+  {
+    id: 83,
+    letters: "clean",
+  },
+  {
+    id: 84,
+    letters: "junk",
+  },
+  {
+    id: 85,
+    letters: "habit",
+  },
+  {
+    id: 86,
+    letters: "fruits",
+  },
+  {
+    id: 87,
+    letters: "vegetables",
+  },
 ]
--- src/store/modules/quizzes.js
+++ src/store/modules/quizzes.js
@@ -18,6 +18,54 @@ const state = () => ({
       isVisible: true,
       questionsId: [17, 18, 19, 20, 21, 22, 23, 24]
     },
+    {
+      id: 4,
+      title: "Spelling Test 4",
+      isVisible: true,
+      questionsId: [25, 26, 27, 28, 29, 30, 31, 32]
+    },
+    {
+      id: 5,
+      title: "Spelling Test 5",
+      isVisible: true,
+      questionsId: [33, 34, 35, 36, 37, 38, 39, 40]
+    },
+    {
+      id: 6,
+      title: "Spelling Test 6",
+      isVisible: true,
+      questionsId: [41, 42, 43, 44, 45, 46, 47, 48]
+    },
+    {
+      id: 7,
+      title: "Spelling Test 7",
+      isVisible: true,
+      questionsId: [49, 50, 51, 52, 53, 54, 55, 56]
+    },
+    {
+      id: 8,
+      title: "Spelling Test 8",
+      isVisible: true,
+      questionsId: [57, 58, 59, 60, 61, 62, 63]
+    },
+    {
+      id: 9,
+      title: "Spelling Test 9",
+      isVisible: true,
+      questionsId: [64, 65, 66, 67, 68, 69, 70, 71]
+    },
+    {
+      id: 10,
+      title: "Spelling Test 10",
+      isVisible: true,
+      questionsId: [72, 73, 74, 75, 76, 77, 78, 79]
+    },
+    {
+      id: 11,
+      title: "Spelling Test 11",
+      isVisible: true,
+      questionsId: [80, 81, 82, 83, 84, 85, 86, 87]
+    },
   ]
 })
 
