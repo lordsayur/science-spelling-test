4e66cded224c229e03bb17c475cff9c35f20260e
Lord Sayur <umar_317@hotmail.com>
2021-02-23 16:34:40 +0800

    feat: add enum and assets path alias

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,8 @@ module.exports = {
         "@": path.resolve(__dirname, 'src'),
         "@components": path.resolve(__dirname, 'src/components'),
         "@composables": path.resolve(__dirname, 'src/composables'),
+        "@enums": path.resolve(__dirname, 'src/enums'),
+        "@assets": path.resolve(__dirname, 'src/assets'),
       },
       extensions: ['.js', '.vue', '.json']
     }
