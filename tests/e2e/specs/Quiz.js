0a640c400861365fb34f7727cb6b72c20c29cb14
Lord Sayur <umar_317@hotmail.com>
2021-02-23 21:23:50 +0800

    test: add Quiz Test

---
+++ tests/e2e/specs/Quiz.js
@@ -0,0 +1,43 @@
+describe('Quiz', () => {
+  it('Shoud display warning message if user click check button with empty input', () => {
+    cy.visit('/quiz/1')
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#message').should((message) => {
+      expect(message).to.contain('Make sure fill all missing letters.')
+    })
+  })
+  it('Shoud display error message if user click check button with incorrect input', () => {
+    cy.visit('/quiz/1')
+    cy.get('input').type('h')
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#message').should((message) => {
+      expect(message).to.contain('Please try again')
+    })
+  })
+  it('Shoud display valid message if user click check button with correct input', () => {
+    cy.visit('/quiz/1')
+    cy.get('input').type('H')
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#message').should((message) => {
+      expect(message).to.contain('Yayy')
+    })
+  })
+  it('Shoud display next question if user click next button', () => {
+    cy.visit('/quiz/1')
+    cy.get('input').type('H')
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#question-number').should((message) => {
+      expect(message).to.contain('2')
+    })
+  })
+  it('Shoud display sorry message if user unable to answer within time limit', () => {
+    cy.visit('/quiz/1')
+    cy.get('input').type('H')
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#message', { timeout: 10000 }).should((message) => {
+      expect(message).to.contain('Sorry...')
+    })
+  })
+})
