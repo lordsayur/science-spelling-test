2104a36df684d3a432623b5c73ef105cd094e13f
Lord Sayur <umar_317@hotmail.com>
2021-02-23 20:39:42 +0800

    feat: add use quiz machine composable

---
+++ src/composables/useQuizMachine.js
@@ -0,0 +1,70 @@
+import { Machine, assign, interpret } from "xstate";
+import { useService } from "@xstate/vue";
+
+import QuizMachineEnum from '@enums/quizMachine';
+import AudioMachineEnum from '@enums/audio';
+
+const quizMachine = Machine({
+  id: "quiz",
+  type: "parallel",
+  states: {
+    quizState: {
+      initial: QuizMachineEnum.state.ANSWERING,
+      context: {
+        timer: 0,
+      },
+      states: {
+        answering: {
+          entry: assign({
+            timer: (context, event) => context.timer + 1,
+          }),
+          on: {
+            CORRECT: QuizMachineEnum.state.QUESTION_FINISH,
+            TIMEOUT: QuizMachineEnum.state.QUESTION_FINISH,
+          },
+        },
+        questionFinish: {
+          on: {
+            NEXT_QUESTION: QuizMachineEnum.state.ANSWERING,
+            COMPLETE: QuizMachineEnum.state.QUIZ_FINISH,
+          },
+        },
+        quizFinish: {
+          type: "final",
+        },
+      },
+    },
+    audioState: {
+      initial: AudioMachineEnum.state.STOP,
+      states: {
+        [AudioMachineEnum.state.PLAYING]: {
+          on: {
+            [AudioMachineEnum.transition.END]: AudioMachineEnum.state.STOP,
+          },
+        },
+        [AudioMachineEnum.state.STOP]: {
+          on: {
+            [AudioMachineEnum.transition.PLAY]: AudioMachineEnum.state.PLAYING,
+          },
+        },
+      },
+    },
+  },
+  actions: {
+    startTimer: (context, event) => {
+      let count = 20;
+      let timer = setInterval(() => {
+        count--;
+        if (count < 1) {
+          clearInterval(timer);
+        }
+      }, 1000);
+    },
+  },
+});
+
+const service = interpret(quizMachine).start();
+
+export const useQuizMachine = () => {
+  return useService(service)
+}
