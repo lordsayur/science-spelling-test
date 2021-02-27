24126264b359a883f283141609096d339d392336
Lord Sayur <umar_317@hotmail.com>
2021-02-27 12:11:14 +0800

    test: update element reference

--- tests/e2e/specs/Quiz.js
+++ tests/e2e/specs/Quiz.js
@@ -1,7 +1,7 @@
 describe('Quiz', () => {
   it('Shoud display warning message if user click check button with empty input', () => {
     cy.visit('/quiz/1')
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#checkBtn').click()
     cy.get('#message').should((message) => {
       expect(message).to.contain('Make sure fill all missing letters.')
     })
@@ -9,7 +9,7 @@ describe('Quiz', () => {
   it('Shoud display error message if user click check button with incorrect input', () => {
     cy.visit('/quiz/1')
     cy.get('input').type('h')
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#checkBtn').click()
     cy.get('#message').should((message) => {
       expect(message).to.contain('Please try again')
     })
@@ -17,7 +17,7 @@ describe('Quiz', () => {
   it('Shoud display valid message if user click check button with correct input', () => {
     cy.visit('/quiz/1')
     cy.get('input').type('H')
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#checkBtn').click()
     cy.get('#message').should((message) => {
       expect(message).to.contain('Yayy')
     })
@@ -25,8 +25,8 @@ describe('Quiz', () => {
   it('Shoud display next question if user click next button', () => {
     cy.visit('/quiz/1')
     cy.get('input').type('H')
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#checkBtn').click()
+    cy.get('#nextBtn').click()
     cy.get('#question-number').should((message) => {
       expect(message).to.contain('2')
     })
@@ -34,8 +34,8 @@ describe('Quiz', () => {
   it('Shoud display sorry message if user unable to answer within time limit', () => {
     cy.visit('/quiz/1')
     cy.get('input').type('H')
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
-    cy.get(':nth-child(3) > .flex-col > div.flex').click()
+    cy.get('#checkBtn').click()
+    cy.get('#checkBtn').click()
     cy.get('#message', { timeout: 10000 }).should((message) => {
       expect(message).to.contain('Sorry...')
     })
