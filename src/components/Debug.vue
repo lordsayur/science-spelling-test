42ef55ec89e9c2238c7ad190dad2eda72c65f80c
Lord Sayur <umar_317@hotmail.com>
2021-02-28 22:20:54 +0800

    feat: add debug component

---
+++ src/components/Debug.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="debug" v-if="env == 'development'">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Debug",
+
+  setup() {
+    return {
+      env: process.env.NODE_ENV,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
