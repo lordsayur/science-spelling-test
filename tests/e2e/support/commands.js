8ca73ea6fa103aed32c49777338cbf9d1bb24f89
Lord Sayur <umar_317@hotmail.com>
2021-02-28 22:29:29 +0800

    test: add verify message command

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("verifyMessage", (element, expectedMessage) => {
+  cy.get(element).should((message) => {
+    expect(message).to.contain(expectedMessage)
+  })
+})
